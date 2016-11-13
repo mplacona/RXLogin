593ee2c9ec6589256ab95ead0a21e6ddb8218484
mplacona <marcos.placona@gmail.com>
2016-11-13 11:47:50 +0100

    Add bindings for drawables

--- app/src/main/java/uk/co/placona/rxlogin/MainActivity.java
+++ app/src/main/java/uk/co/placona/rxlogin/MainActivity.java
@@ -2,7 +2,6 @@ package uk.co.placona.rxlogin;
 
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -10,6 +9,7 @@ import android.widget.EditText;
 
 import com.jakewharton.rxbinding.widget.RxTextView;
 
+import butterknife.BindDrawable;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import rx.Observable;
@@ -25,28 +25,31 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.login_bt)
     Button mLoginButton;
 
+    @BindDrawable(android.R.drawable.presence_busy)
+    Drawable mInvalidField;
+
+    @BindDrawable(android.R.drawable.presence_online)
+    Drawable mValidField;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ButterKnife.bind(this);
+ButterKnife.bind(this);
 
-        Drawable invalidField = ContextCompat.getDrawable(getApplicationContext(), android.R.drawable.presence_busy);
-        Drawable validField = ContextCompat.getDrawable(getApplicationContext(), android.R.drawable.presence_online);
+Observable<CharSequence> loginObservable = RxTextView.textChanges(mLogin);
+loginObservable
+        .map(this::isValidLogin)
+        .subscribe(isValid -> mLogin.setCompoundDrawablesRelativeWithIntrinsicBounds(null,null, (isValid ? mValidField : mInvalidField), null));
 
-        Observable<CharSequence> loginObservable = RxTextView.textChanges(mLogin);
-        loginObservable
-                .map(this::isValidLogin)
-                .subscribe(isValid -> mLogin.setCompoundDrawablesRelativeWithIntrinsicBounds(null,null, (isValid ? validField : invalidField), null));
+Observable<CharSequence> passwordObservable = RxTextView.textChanges(mPassword);
+passwordObservable
+        .map(this::isValidPassword)
+        .subscribe(isValid -> mPassword.setCompoundDrawablesRelativeWithIntrinsicBounds(null,null, (isValid ? mValidField : mInvalidField), null));
 
-        Observable<CharSequence> passwordObservable = RxTextView.textChanges(mPassword);
-        passwordObservable
-                .map(this::isValidPassword)
-                .subscribe(isValid -> mPassword.setCompoundDrawablesRelativeWithIntrinsicBounds(null,null, (isValid ? validField : invalidField), null));
-
-        Observable<Boolean> combinedObservables = Observable.combineLatest(loginObservable, passwordObservable, (o1, o2) -> isValidLogin(o1) && isValidPassword(o2));
-        combinedObservables.subscribe(isVisible -> mLoginButton.setVisibility(isVisible ? View.VISIBLE : View.GONE));
+Observable<Boolean> combinedObservables = Observable.combineLatest(loginObservable, passwordObservable, (o1, o2) -> isValidLogin(o1) && isValidPassword(o2));
+combinedObservables.subscribe(isVisible -> mLoginButton.setVisibility(isVisible ? View.VISIBLE : View.GONE));
     }
 
     private boolean isValidPassword(CharSequence value) {
@@ -54,7 +57,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private boolean isValidLogin(CharSequence value) {
-        return value.length() >= 5;
+        //return value.length() >= 5;
+        return value.toString().matches("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])");
     }
 
 
